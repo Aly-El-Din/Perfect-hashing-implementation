5ab3d1fa87e3cb85482128795c157ea69b86997a
Aly El Din <eng.alyeldin@gmail.com>
2024-04-18 01:53:15 +0200

    Universal hashing

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        UniversalHashing universalHashing=new UniversalHashing(3,4);
+        universalHashing.computeIndex(6);
+
+    }
+}
