8f1a7f404766bed9cd14aa7292c17f2495b4752b
Aly El Din <eng.alyeldin@gmail.com>
2024-04-18 21:44:01 +0200

    Modified universal hash

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        UniversalHashing universalHashing=new UniversalHashing(7,4);
+        universalHashing.computeIndex(5);
+
+
+    }
+}
