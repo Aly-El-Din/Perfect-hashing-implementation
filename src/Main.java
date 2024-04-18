40d197df55e6bc381cd4200a2e3d417e060a5268
Aly El Din Mohamed El Sayed <124249146+Aly-El-Din@users.noreply.github.com>
2024-04-18 21:39:26 +0200

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        UniversalHashing universalHashing=new UniversalHashing(3,4);
-        universalHashing.computeIndex(6);
-
-    }
-}
