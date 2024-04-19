32926b0e4c784b50db447893c389f9d410c97984
Amir-Ragaie <amir.ragaie.soliman@gmail.com>
2024-04-19 23:40:27 +0200

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,21 +3,15 @@ package com.example;
 import java.util.HashMap;
 import java.util.Random;
 
-
-
-
-
-
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         int[] k = new int[1000];
 // outputs collisions to a file
-        for (int i = 0; i < 1000;i++){
-        ////
+        for (int i = 0; i < 1000; i++) {
+            ////
             Random rand = new Random();
             int n = rand.nextInt(1000000);
             k[i] = n;
@@ -26,7 +20,7 @@ public class App
                 continue;
             }
             map.put(n, i);
-           }            
-        
+        }
+        System.out.println("Done");
     }
 }
