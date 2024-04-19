5e33770258cfc1f921cef2332a5e1f192af9fc97
abdalllah911 <abdallahadel667@gmail.com>
2024-04-19 22:44:05 +0200

    aa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         UniversalHashing universalHashing=new UniversalHashing(7,4);
         universalHashing.computeIndex(5);
-
+   
 
     }
 }
