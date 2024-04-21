287921ae697a0b82c9be6acbd8f0f6f8523c9606
Amir-Ragaie <amir.ragaie.soliman@gmail.com>
2024-04-22 01:51:34 +0200

    Test updates
    add total collisions and search time

--- src/test/java/com/example/CSVWriter.java
+++ src/test/java/com/example/CSVWriter.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 
 public class CSVWriter {
 
-    public void writeData(String fileName, int inputSize, int sampleSize, long[] averageTimesPerInsertion,
-            int[] spaceComplexities) {
+    public void writeData(String fileName, int inputSize, int sampleSize, long[] averageTimesPerInsertion, long[] averageTimesPerSearch, int[] totalCollisions, int[] spaceComplexities) {
+
         try {
             FileWriter writer = new FileWriter(fileName);
             
             writer.append("Average Time Per Insertion");
+            writer.append(",");
+            writer.append("Average Time Per Search");
+            writer.append(",");
+            writer.append("Total Collisions");
             writer.append(",");
             writer.append("Space Complexity");
             writer.append("\n");
@@ -18,6 +22,10 @@ public class CSVWriter {
             for (int i = 0; i < sampleSize; i++) {
               
                 writer.append(String.valueOf(averageTimesPerInsertion[i]));
+                writer.append(",");
+                writer.append(String.valueOf(averageTimesPerSearch[i]));
+                writer.append(",");
+                writer.append(String.valueOf(totalCollisions[i]));
                 writer.append(",");
                 writer.append(String.valueOf(spaceComplexities[i]));
                 writer.append("\n");
--- src/test/java/com/example/SamplingAndComparisonTest.java
+++ src/test/java/com/example/SamplingAndComparisonTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import static org.junit.Assume.assumeNoException;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class SamplingAndComparisonTest {
@@ -12,26 +13,42 @@ public class SamplingAndComparisonTest {
     @Test
     public void complexityAnalysisTest() {
         int space = 0; // space complexity
-        long totalTime = 0; // total time taken to insert entries
+        long totalInsertionTime = 0; // total time taken to insert entries
+        long totalSearchTime = 0; // total time taken to search entries
         int sampleSize = 5; // number of samples to be taken of a given input size
         int inputSize = 1000; // random input size (size of entries to be inserted)
         int c = new Random().nextInt(9) + 1; // random constant c assumed to be between 1 - 9
         int maxInputSize = 10001; // maximum input size
         int[] spaceComplexities = new int[sampleSize];
         long[] averageTimesPerInsertion = new long[sampleSize];
+        long[] averageTimesPerSearch = new long[sampleSize];
+        ArrayList<String> values = new ArrayList<>();
+        int[] totalCollisions = new int[sampleSize];
 
         for (int x = inputSize; x < maxInputSize; x += 1000) {
 
             for (int i = 0; i < sampleSize; i++) {
                 HashingN<String> hash = new HashingN<>(2 * inputSize);
-                long start = System.currentTimeMillis();
+                long startInsert = System.currentTimeMillis();
 
-                for (int j = 0; j < x; j++)
-                    hash.insert(new Generator().generateString());
+                Generator generator = new Generator();
+                for (int j = 0; j < x; j++){
+                    String word = generator.generateString();
+                    hash.insert(word);
+                    values.add(word);
+                }
+                long endInsert = System.currentTimeMillis();
 
-                long end = System.currentTimeMillis();
+                totalInsertionTime = (endInsert - startInsert);
 
-                totalTime = (end - start);
+                long startSearch = System.currentTimeMillis();
+
+                for (int j = 0; j < x; j++){
+                    hash.search(values.get(j));
+                }
+                long endSearch = System.currentTimeMillis();
+
+                totalSearchTime = (endSearch - startSearch);
 
                 int[] N = hash.getN();
                 for (int k = 0; k < 2 * inputSize; k++) {
@@ -39,17 +56,21 @@ public class SamplingAndComparisonTest {
                 } 
 
                 // Calculate average time per insertion
-                averageTimesPerInsertion[i] = totalTime;
+                averageTimesPerInsertion[i] = totalInsertionTime;
+                averageTimesPerSearch[i] = totalSearchTime;
 
                 // Calculate space complexity
                 
                 spaceComplexities[i] = space + 2 * inputSize;
+
+                // Calculate total collisions
+                totalCollisions[i] = hash.getInnerCollisions() ;
             }
 
             // Write data to CSV file
             try {
                 new CSVWriter().writeData("data".concat(String.valueOf(x).concat(".csv")), inputSize, sampleSize,
-                        averageTimesPerInsertion, spaceComplexities);
+                        averageTimesPerInsertion,averageTimesPerSearch,totalCollisions, spaceComplexities);
             } catch (Exception e) {
                 e.printStackTrace();
             }
